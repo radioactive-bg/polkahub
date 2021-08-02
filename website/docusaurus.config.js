9a0609e2603cef1bf064eed948dad11ed977e92c
Mihail Petkov <mihailpetkovv@abv.bg>
2021-08-02 17:43:32 +0300

    update config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -107,13 +107,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/radioactive-bg/polkahub/edit/master/website/',
+            'https://github.com/radioactive-bg/polkahub/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/radioactive-bg/polkahub/edit/master/website/blog/',
+            'https://github.com/radioactive-bg/polkahub/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
